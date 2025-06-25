b8981a667860f6f7fdaa5ba55d8f5f6d9ea5ba30
Bravert09 <1478499573@qq.com>
2025-06-25 15:24:44 +0800

    fix:use process.env.PORT for Render

--- ai-proxy-server/index.js
+++ ai-proxy-server/index.js
@@ -6,7 +6,8 @@ import { getRecipeFromDeepSeek } from './getRecipeFromDeepSeek.js'
 dotenv.config()
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
+
 
 app.use(cors())
 app.use(express.json())
